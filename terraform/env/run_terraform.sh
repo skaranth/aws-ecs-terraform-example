1d72e2da992a297e1f340a876eea173eeb6fdee3
sharan.karanth@gmail.com <sharan.karanth@gmail.com>
2020-06-15 14:42:59 -0400

    Fixed issue with terragrunt not finding the var file

--- terraform/env/run_terraform.sh
+++ terraform/env/run_terraform.sh
@@ -3,17 +3,18 @@ ENV=$1
 MODULE=$2
 
 REGION=us-east-1
-BASEDIR=$(dirname $0)
-TF_ROOT_DIR=$BASEDIR/modules
-RUN_DIR=$TF_ROOT_DIR/$MODULE
+BASEDIR=$PWD/$(dirname $0)
+TG_ROOT_DIR=$BASEDIR/modules
+RUN_DIR=$TG_ROOT_DIR/$MODULE
 
 TEMPLATE_FILE=$BASEDIR/terragrunt_template
 
-TERRAGRUNT_FILE=$TF_ROOT_DIR/terragrunt.hcl
+TERRAGRUNT_FILE=$TG_ROOT_DIR/terragrunt.hcl
 VAR_FILE=$BASEDIR/vars/$ENV
+TARGET_VAR_FILE=$TG_ROOT_DIR/env.tfvars
 
 clean(){
-  rm -rf $TF_ROOT_DIR/terraform.tfvars
+  rm -rf $TARGET_VAR_FILE
   rm -rf $TERRAGRUNT_FILE
   rm -rf $RUN_DIR/.terragrunt_cache
 }
@@ -26,11 +27,13 @@ setup_template(){
   sed -i.bak "s/{{THE_BUCKET}}/$ENV-$MODULE/g" $TERRAGRUNT_FILE
   sed -i.bak "s/{{THE_LOCK_TABLE}}/$ENV-$MODULE/g" $TERRAGRUNT_FILE
   sed -i.bak "s/{{THE_REGION}}/$REGION/g" $TERRAGRUNT_FILE
+  sed -i.bak "s/{{TERRAGRUNT_ROOT}}/$(echo $TG_ROOT_DIR | sed 's_/_\\/_g')/g" $TERRAGRUNT_FILE
   rm -rf $TERRAGRUNT_FILE.bak
 }
+
 setup_vars(){
-  echo "Setting up vars for env:$ENV"
-  cp $VAR_FILE $TF_ROOT_DIR/terraform.tfvars
+  echo "Setting up vars file:$TARGET_VAR_FILE"
+  cp $VAR_FILE $TG_ROOT_DIR/env.tfvars
 }
 
 
@@ -43,3 +46,4 @@ clean
 setup_template
 setup_vars
 run
+clean
